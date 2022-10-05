9b5f32ccde86a03c0b022dc56c3339b9ad3ca87e
KauBoui <errolbucy@gmail.com>
2022-10-04 19:24:59 -0700

    drawing refactor

--- src/picrosspuzzle.py
+++ src/picrosspuzzle.py
@@ -1,5 +1,16 @@
 
 
+def convert_clues(clues):
+    converted = [[],[]]
+    row_col = clues.split('/')
+    for i, item in enumerate(row_col):
+        item = item.split(',')
+        for clue in item:
+            clue = clue.split('.')
+            clue = [int(x) for x in clue]
+            converted[i].append(clue)
+    return converted
+
 class PicrossPuzzle:
 
     def __init__(self, row_clues: "list[tuple]", col_clues: "list[tuple]") -> None:
